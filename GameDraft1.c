2a409cf879260db86ca8b112ae5ff1f3d4e6b238
GianMauricio <44298742+GianMauricio@users.noreply.github.com>
2019-02-21 08:49:53 +0800

    Update GameDraft1.c

--- GameDraft1.c
+++ GameDraft1.c
@@ -27,11 +27,6 @@ void main()
 		switch (nTimeRemaining)
 		{
 			case 1: 
-				char content[] = "There is a cat stucked in a burning tree.";
-				char q1[] = "What do you do?";
-				char a1[] = "1. Jump out of the window";
-				char a2[] = "2. Run down as quick as fuck down the stairs";
-				char q2[] = "You're already in your lawn where the burning tree is. You see a hose to your left and a dog to your right. ";
 				
 				TypeWithColor(15, 50, content); 
 				printf("\n");
